0d877b516b3fff0041502d69f735e15b94cd30d4
mekdes wale <mekdesw60@gmail.com>
2026-08-01 13:18:06 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600;700&display=swap" 
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
